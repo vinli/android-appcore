e82607b1da13724c3176a2ff13ad9ef6171a76f0
Christopher Casey <cmc@vin.li>
2015-12-10 16:07:06 -0600

    add reusable fragment container id utility to FragmentService

--- android-appcore/src/main/java/li/vin/appcore/mortarflow/android/FragmentService.java
+++ android-appcore/src/main/java/li/vin/appcore/mortarflow/android/FragmentService.java
@@ -1,18 +1,52 @@
 package li.vin.appcore.mortarflow.android;
 
 import android.content.Context;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.View;
 import java.lang.ref.WeakReference;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import li.vin.appcore.R;
 import mortar.MortarScope;
 import mortar.Scoped;
 
 @SuppressWarnings("unused")
 public final class FragmentService implements Scoped {
   public static final String SERVICE_NAME = FragmentService.class.getName();
+
+  private static int reuseFragmentIndex;
+
+  private static final List<Integer> reusableIds = Collections.unmodifiableList(Arrays.asList( //
+      R.id.reusable_fragment_container_0, //
+      R.id.reusable_fragment_container_1, //
+      R.id.reusable_fragment_container_2, //
+      R.id.reusable_fragment_container_3, //
+      R.id.reusable_fragment_container_4, //
+      R.id.reusable_fragment_container_5, //
+      R.id.reusable_fragment_container_6, //
+      R.id.reusable_fragment_container_7, //
+      R.id.reusable_fragment_container_8, //
+      R.id.reusable_fragment_container_8, //
+      R.id.reusable_fragment_container_9, //
+      R.id.reusable_fragment_container_10, //
+      R.id.reusable_fragment_container_11, //
+      R.id.reusable_fragment_container_12, //
+      R.id.reusable_fragment_container_13, //
+      R.id.reusable_fragment_container_14, //
+      R.id.reusable_fragment_container_15 //
+  ));
+
+  public static int getFragmentContainerReusableId() {
+    if (Thread.currentThread() != Looper.getMainLooper().getThread()) {
+      throw new RuntimeException("must be on ui thread.");
+    }
+    return reusableIds.get(reuseFragmentIndex = ((reuseFragmentIndex + 1) % reusableIds.size()));
+  }
 
   public static FragmentService getFragmentService(Context context) {
     //noinspection ResourceType
