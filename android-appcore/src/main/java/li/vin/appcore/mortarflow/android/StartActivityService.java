48e51fcf9e05c676445b441cc8d6bf6c11c826c9
Christopher Casey <cmc@vin.li>
2015-12-10 12:19:32 -0600

    add better utility methods to StartActivityService

--- android-appcore/src/main/java/li/vin/appcore/mortarflow/android/StartActivityService.java
+++ android-appcore/src/main/java/li/vin/appcore/mortarflow/android/StartActivityService.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
 import java.lang.ref.WeakReference;
 import mortar.MortarScope;
 import mortar.Scoped;
 
+@SuppressWarnings("unused")
 public final class StartActivityService implements Scoped {
   public static final String SERVICE_NAME = StartActivityService.class.getName();
 
@@ -19,6 +22,55 @@ public final class StartActivityService implements Scoped {
 
   public static StartActivityService getStartActivityService(MortarScope scope) {
     return scope.getService(SERVICE_NAME);
+  }
+
+  @Nullable
+  public static MortarFlowAppCompatActivity getActivity(@NonNull Context context) {
+    try {
+      return getStartActivityService(context).getActivity();
+    } catch (Exception e) {
+      Log.e(SERVICE_NAME, "failed getActivity", e);
+    }
+    return null;
+  }
+
+  @Nullable
+  public static MortarFlowAppCompatActivity getActivity(@NonNull View view) {
+    return getActivity(view.getContext());
+  }
+
+  public static boolean startActivityForResult(@NonNull Context context, //
+      Intent intent, int requestCode, @Nullable Bundle options) {
+    try {
+      getStartActivityService(context).startActivityForResult(intent, requestCode, options);
+      return true;
+    } catch (Exception e) {
+      Log.e(SERVICE_NAME, "failed startActivityForResult " + //
+          intent + " " + requestCode + " " + options, e);
+    }
+    return false;
+  }
+
+  public static boolean startActivityForResult(@NonNull View view, //
+      Intent intent, int requestCode, @Nullable Bundle options) {
+    return startActivityForResult(view.getContext(), intent, requestCode, options);
+  }
+
+  public static boolean startActivity(@NonNull Context context, //
+      Intent intent, @Nullable Bundle options) {
+    try {
+      getStartActivityService(context).startActivity(intent, options);
+      return true;
+    } catch (Exception e) {
+      Log.e(SERVICE_NAME, "failed startActivity " + //
+          intent + " " + options, e);
+    }
+    return false;
+  }
+
+  public static boolean startActivity(@NonNull View view, //
+      Intent intent, @Nullable Bundle options) {
+    return startActivity(view.getContext(), intent, options);
   }
 
   private WeakReference<MortarFlowAppCompatActivity> activityRef = new WeakReference<>(null);
