b3aec90cbdaa1537e8c425aab3c9d47379250a35
Christopher Casey <cmc@vin.li>
2015-12-10 14:59:55 -0600

    add better utility methods to FragmentService

--- android-appcore/src/main/java/li/vin/appcore/mortarflow/android/FragmentService.java
+++ android-appcore/src/main/java/li/vin/appcore/mortarflow/android/FragmentService.java
@@ -2,11 +2,15 @@ package li.vin.appcore.mortarflow.android;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.View;
 import java.lang.ref.WeakReference;
 import mortar.MortarScope;
 import mortar.Scoped;
 
+@SuppressWarnings("unused")
 public final class FragmentService implements Scoped {
   public static final String SERVICE_NAME = FragmentService.class.getName();
 
@@ -17,6 +21,21 @@ public final class FragmentService implements Scoped {
 
   public static FragmentService getFragmentService(MortarScope scope) {
     return scope.getService(SERVICE_NAME);
+  }
+
+  @Nullable
+  public static FragmentManager getFragmentManager(@NonNull Context context) {
+    try {
+      return getFragmentService(context).getFragmentManager();
+    } catch (Exception e) {
+      Log.e(SERVICE_NAME, "failed getFragmentManager", e);
+    }
+    return null;
+  }
+
+  @Nullable
+  public static FragmentManager getFragmentManager(@NonNull View view) {
+    return getFragmentManager(view.getContext());
   }
 
   private WeakReference<FragmentManager> fragmentManagerRef = new WeakReference<>(null);
