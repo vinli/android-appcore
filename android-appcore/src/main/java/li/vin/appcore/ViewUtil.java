6d6f6e114fa8be9b3af950c2b055873620c145c6
Christopher Casey <cmc@vin.li>
2015-12-20 09:22:17 -0600

    add set typeface util with caching to avoid older android version memleaks

--- android-appcore/src/main/java/li/vin/appcore/ViewUtil.java
+++ android-appcore/src/main/java/li/vin/appcore/ViewUtil.java
@@ -1,15 +1,23 @@
 package li.vin.appcore;
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.ViewTreeObserver.OnPreDrawListener;
+import android.widget.TextView;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by christophercasey on 9/3/15.
  */
+@SuppressWarnings("unused")
 public final class ViewUtil {
 
   public interface OnMeasuredCallback {
@@ -54,6 +62,39 @@ public final class ViewUtil {
       }
     }
     return outSet;
+  }
+
+  /** Always cache created Typefaces to avoid mem leaks on older Android versions. */
+  private static final class TypefaceCacheHolder {
+    private static final Map<String, Typeface> fontCache = new HashMap<>();
+  }
+
+  @NonNull
+  public static Typeface getTypeface(@NonNull Context context, @NonNull String assetPath) {
+    assertUiThread();
+    Typeface tf = TypefaceCacheHolder.fontCache.get(assetPath);
+    if (tf == null) {
+      TypefaceCacheHolder.fontCache.put(assetPath,
+          tf = Typeface.createFromAsset(context.getApplicationContext().getAssets(), assetPath));
+    }
+    if (tf == null) {
+      throw new NullPointerException("null tf, should never happen.");
+    }
+    return tf;
+  }
+
+  public static void setTypeface(@NonNull TextView textView, @NonNull String assetPath, int style) {
+    textView.setTypeface(getTypeface(textView.getContext(), assetPath), style);
+  }
+
+  public static void setTypeface(@NonNull TextView textView, @NonNull String assetPath) {
+    textView.setTypeface(getTypeface(textView.getContext(), assetPath));
+  }
+
+  private static void assertUiThread() {
+    if (Thread.currentThread() != Looper.getMainLooper().getThread()) {
+      throw new RuntimeException("requires ui thread");
+    }
   }
 
   private ViewUtil() {
